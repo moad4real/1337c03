bccaf4b0decec4482f2c25f5ad8c3a3bbecbfcb3
moad <kharoubamouad@gmail.com>
2023-10-02 11:30:29 +0100

    dddd

---
+++ ex02/ft_strcat.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+char	*ft_strcat(char*dest,char*src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[i])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
